13010cef106e166b830454bb02bc85a95cbf6086
Alioune <alioune1117@gmail.com>
2020-05-21 17:07:27 +0200

    tag script in grigio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Raccolta dati
+
+var name = document.getElementById('form_name');
+var km = document.getElementById('form_km');
+var age = document.getElementById('form_age');
+var passenger = document.getElementById('passenger_name');
+
+alert("ciao");
