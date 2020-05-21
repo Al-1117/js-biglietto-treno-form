278daa1ea128e86647d820a2aa9e1d242bb52cf7
Alioune <alioune1117@gmail.com>
2020-05-21 19:55:42 +0200

    provato a completare la parte logica ma non funziona per errore di sintassi ,credo.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,56 @@
 // Raccolta dati
 
 var name = document.getElementById('form_name');
-var km = document.getElementById('form_km');
+var km = parseInt(document.getElementById('form_km'));
 var age = document.getElementById('form_age');
-var passenger = document.getElementById('passenger_name');
 
-alert("ciao");
+
+// Altre variabili
+
+var ageValue;
+var kmValue;
+
+var passengerName = document.getElementById('passenger_name');
+
+var buttonGenera = document.getElementById('ms_btn_genera');
+
+var buttonAnnulla = document.getElementById('ms_btn_annulla');
+
+// Evento cliccando Tasto Genera
+buttonGenera.addEventListener('click',
+  function() {
+    // Nome passeggero sul biglietto
+    passengerName.innerHTML = name.value;
+    // Dati inseriti dall'utente
+    ageValue = age.value;
+    kmValue = km.value;
+  }
+
+);
+
+// Evento cliccando tasto annulla
+
+buttonAnnulla.addEventListener('click',
+  function() {
+    // Reset dei dati
+    name.value = '';
+    passengerName.innerHTML = '';
+    kmValue.innerHTML = '';
+
+  }
+)
+
+
+// Prezzo
+
+var ticketPrice = km * 0.21;
+
+if (age == 'minorenne') {
+  ticketPrice = ticketPrice - (ticketPrice * 20 / 100);
+
+} else if (age == 'over'){
+  ticketPrice = ticketPrice - (ticketPrice * 40 / 100);
+
+}
+
+console.log(ticketPrice);
