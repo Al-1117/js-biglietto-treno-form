9c8073f589b30dc39c28d2740e2bfbaf1fb2dfab
Alioune <alioune1117@gmail.com>
2020-05-24 12:25:06 +0200

    aggiunto gestione dell'errore dati immessi dall'utente.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ var buttonAnnulla = document.getElementById('ms_btn_annulla');
 
 var ticketDetails = document.getElementById('details');
 
+var mistake = document.getElementById('mistake');
+
+// Varibili prezzo Biglietto
+var kmInputValue;
+var inputAgeValue;
 
 
 // Variabili dati Biglietto
@@ -22,42 +27,54 @@ var carrozza = document.getElementById('chariot');
 var cp = document.getElementById('cp');
 var costoBiglietto = document.getElementById('ticket_cost');
 
-// Varibili prezzo Biglietto
-var kmInputValue;
-var inputAgeValue;
 
 // Evento cliccando Tasto Genera
 buttonGenera.addEventListener('click',
   function() {
 
-    ticketDetails.className = ticketDetails.classlist + " display_block"
-
-
-    // Nome passeggero sul biglietto
-    passengerName.innerHTML = nameInput.value;
     // Dati inseriti dall'utente
-    kmInputValue = kmInput.value;
+    kmInputValue = parseInt(kmInput.value);
     inputAgeValue = ageInput.value;
 
-    // Prezzo Biglietto e calcolo
-    var ticketPrice = kmInputValue * 0.21;
+    console.log(kmInputValue);
 
+    // Controllo errore input utente
 
-    if (inputAgeValue == 'minorenne') {
-      ticketPrice = ticketPrice - (ticketPrice * 20 / 100);
-      discount.innerHTML = "Offerta minorenne";
-
-    } else if (inputAgeValue == 'over'){
-      ticketPrice = ticketPrice - (ticketPrice * 40 / 100);
-      discount.innerHTML = "Offerta over 65";
+    if( isNaN(kmInputValue) || nameInput.lenght == 0 ) {
+      mistake.className = 'display_block';
+      ticketDetails.className = ticketDetails.classlist + " display_none";
 
     }
+    else {
 
-    // stampo dati elaborati
-    costoBiglietto.innerHTML = ticketPrice.toFixed(2) + " euro";
-    carrozza.innerHTML = Math.floor(Math.random() * 21);
-    cp.innerHTML = Math.floor(Math.random() * 10000);
+      mistake.className = 'display_none';
 
+      // Mostro il biglietto
+      ticketDetails.className = ticketDetails.classlist + " display_block"
+
+      // Nome passeggero sul biglietto
+      passengerName.innerHTML = nameInput.value;
+
+      // Prezzo Biglietto e calcolo
+      var ticketPrice = kmInputValue * 0.21;
+
+
+      if (inputAgeValue == 'minorenne') {
+        ticketPrice = ticketPrice - (ticketPrice * 20 / 100);
+        discount.innerHTML = "Offerta minorenne";
+
+      } else if (inputAgeValue == 'over'){
+        ticketPrice = ticketPrice - (ticketPrice * 40 / 100);
+        discount.innerHTML = "Offerta over 65";
+
+      }
+
+      // stampo dati elaborati
+      costoBiglietto.innerHTML = ticketPrice.toFixed(2) + " euro";
+      carrozza.innerHTML = Math.floor(Math.random() * 21);
+      cp.innerHTML = Math.floor(Math.random() * 10000);
+
+    }
   }
 );
 
@@ -66,8 +83,10 @@ buttonGenera.addEventListener('click',
 buttonAnnulla.addEventListener('click',
   function() {
 
-    // Nascondo Biglietto
-    ticketDetails.className = ticketDetails.classlist + " display_none"
+    // Nascondo Biglietto ed errore
+    ticketDetails.className = ticketDetails.classlist + " display_none";
+
+    mistake.className = 'display_none';
 
 
     // Reset dei dati form
